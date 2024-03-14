959c48dab20edd5cf11e839d88657249ed6e65d2
GabrielDko <gabriielrobless@gmail.com>
2024-03-14 05:14:05 -0300

    completo sin bonus track

--- public/js/moviesAddValidation.js
+++ public/js/moviesAddValidation.js
@@ -1,132 +1,103 @@
-window.onload = function () {
-    // Definir variables globales para controlar los errores
-    let errors = false;
 
-    let titulo = document.querySelector('.moviesAddTitulo')
-    let formulario = document.querySelector('#formulario');
+window.onload = function () {
+    let titulo = document.querySelector('.moviesAddTitulo');
+    let sectionForm = document.querySelector('#formulario');
     let article = document.querySelector('article');
     titulo.innerHTML = 'AGREGAR PELÍCULA';
     titulo.classList.add('titulo');
     article.classList.add('fondoTransparente');
-    formulario.classList.add('fondoCRUD');
+    sectionForm.classList.add('fondoCRUD');
+    const title = document.querySelector('#title');
+    const inputs = document.querySelectorAll('.input');
+    const form = document.querySelector('form');
 
-    const form = document.querySelector('form')
+    title.focus();
 
-    const movieRating = document.querySelector('#rating');
-    const movieAwards = document.querySelector('#awards');
-    const movieReleaseDate = document.querySelector('#release_date');
-    const movieLength = document.querySelector('#length');
-    const movieGenre = document.querySelector('#genre_id');
 
-    const movieTitle = document.querySelector('#title');
-    movieTitle.focus();
 
-    function validarInput(input, minLength, maxLength, minNum, maxNum) {
-        return function(event) {
-            const valorInput = input.value.trim();
-            let error = '';
-    
-            // Realizar las validaciones necesarias
-            if (valorInput == '' || valorInput == null) {
-                error = 'El campo es obligatorio.';
-            } else {
-                if (minLength && valorInput.length < minLength) {
-                    error = `El texto debe tener al menos ${minLength} caracteres.`;
-                } else if (maxLength && valorInput.length > maxLength) {
-                    error = `El texto no puede tener más de ${maxLength} caracteres.`;
-                }
+    function textError(input, message) {
+        const inputBox = input.parentElement;
+        const child = document.getElementById(`${input.name}-error`);
+        input.classList.add('is-invalid');
+        const textError = document.createElement('p');
+
+        textError.id = `${input.name}-error`;
+        textError.innerText = message;
+        child ? inputBox.replaceChild(textError, child) : inputBox.appendChild(textError);
+    }
+
+    function listError(input,message){
+        const listBox = document.querySelector('.errores');
+        const child = document.getElementById(`${input.name}-errorList`);
+        input.classList.add('is-invalid');
+        const listError = document.createElement('li');
+        listBox.classList.add('alert-warning')
+        listError.id = `${input.name}-errorList`;
+        listError.innerText = message;
+        child ? listBox.replaceChild(listError, child) : listBox.appendChild(listError);
+    }
+
+    function removeError(input) {
+        const childText = document.querySelector(`#${input.name}-error`)
+        const childList = document.querySelector(`#${input.name}-errorList`)
+
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+        childText ? childText.remove() : null
+        childList ? childList.remove() : null
+    }
+
+    function validateValue(input, e) {
+        const textErrorElement = document.getElementById(`${input.name}-error`);
+        if (textErrorElement) {
+            textErrorElement.remove();
+        }
+        const listErrorElement = document.getElementById(`${input.name}-errorList`);
+        if (listErrorElement) {
+            listErrorElement.remove();
+        }
+        if (!input.value.trim()) {
+            const emptyMessage = `El campo ${input.name} no puede estar vacío`;
+            e.type === 'submit' ? listError(input, emptyMessage) : textError(input, emptyMessage);
+            return false;
+        }
+        if (input.name === 'awards' || input.name === 'rating') {
+            const min = 0;
+            const max = 10;
+            if (minMax(input, min, max)) {
+                const minMaxMessage = `El campo ${input.name} debe estar entre los valores ${min} y ${max}`;
+                e.type === 'submit' ? listError(input, minMaxMessage) : textError(input, minMaxMessage);
+                return false;
             }
-    
-            // if (!valorInput) {
-            //     error = 'Este campo es obligatorio.';
-            // } else {
-                if (minNum && parseFloat(valorInput) < minNum) {
-                    error = `El valor debe ser mayor o igual a ${minNum}.`;
-                } else if (maxNum && parseFloat(valorInput) > maxNum) {
-                    error = `El valor debe ser menor o igual a ${maxNum}.`;
-                } else if (valorInput < 0) {
-                    error = 'El valor no puede ser negativo.';
-                }
-            // }
-            
-            // Mostrar mensaje de error
-            const mensajeErrorElement = input.nextElementSibling;
-            mensajeErrorElement.textContent = error;
-    
-            // Agregar o eliminar la clase 'is-invalid' según si hay error o no
-            if (error) {
-                input.classList.add('is-invalid');
-                errors = true; // Actualizar el estado de los errores
-            } else {
-                input.classList.remove('is-invalid');
-                input.classList.add('is-valid');
-                errors = false; // Actualizar el estado de los errores
+        }
+        if (input.name === 'length') {
+            const min = 60;
+            const max = 360;
+            if (minMax(input, min, max)) {
+                const minMaxMessage = `El campo ${input.name} debe estar entre los valores ${min} y ${max}`;
+                e.type === 'submit' ? listError(input, minMaxMessage) : textError(input, minMaxMessage);
+                return false;
             }
-        };
+        }
+        removeError(input);
+        return true;
     }
     
-    function validarFecha(input) {
-        const valorInput = input.value;
-        let error = '';
-        console.log(valorInput);
-        // Validar si el valor ingresado es una fecha válida
-        const fechaIngresada = new Date(valorInput);
-        if (isNaN(fechaIngresada.getTime())) {
-            error = 'La fecha ingresada no es válida.';
-        } else {
-            // Obtener la fecha actual
-            const fechaActual = new Date();
-    
-            // Validar que la fecha ingresada no sea en el futuro
-            if (fechaIngresada > fechaActual) {
-                error = 'La fecha ingresada no puede ser en el futuro.';
-            }
-        }
-        // Mostrar mensaje de error
-        const mensajeErrorElement = input.nextElementSibling;
-        mensajeErrorElement.textContent = error;
-    
-        // Agregar o eliminar la clase 'is-invalid' según si hay error o no
-        if (error) {
-            input.classList.add('is-invalid');
-            errors = true; // Actualizar el estado de los errores
-        } else {
-            input.classList.remove('is-invalid');
-            input.classList.add('is-valid');
-            errors = false; // Actualizar el estado de los errores
-        }
+    function minMax(input, min, max) {
+        return (input.value >= min && input.value <= max);
     }
-    
 
-    movieTitle.addEventListener("input", validarInput(movieTitle, 3, 15));
-    movieTitle.addEventListener("blur", validarInput(movieTitle));
-    
-    movieLength.addEventListener("input", validarInput(movieLength, null, null, 60, 360));
-    movieLength.addEventListener("blur", validarInput(movieLength));
+    inputs.forEach(input => {
+        input.addEventListener('blur', (e) => {
+            validateValue(input,e)
+        })
+    })
+    form.addEventListener('submit', (e) => {
+        inputs.forEach(input => {
+            if (validateValue(input,e) == false) {
+                e.preventDefault();
 
-    movieRating.addEventListener("input", validarInput(movieRating, null, null, 0, 10));
-    movieRating.addEventListener("blur", validarInput(movieRating));
-
-    movieAwards.addEventListener("input", validarInput(movieAwards, null, null, 0, 10));
-    movieAwards.addEventListener("blur", validarInput(movieAwards));
-
-
-    movieReleaseDate.addEventListener("blur", validarFecha(movieReleaseDate));
-
-
-    movieGenre.addEventListener('input', validarInput(movieGenre));
-    movieGenre.addEventListener('blur', validarInput(movieGenre));
-
-    form.addEventListener('submit', (event) => {
-        if(errors){
-            event.preventDefault();
-        }
-    });
+            }
+        })
+    })
 }
-
-
-// if (minNum !== undefined && valorInput < minNum) {
-//     error = `El número debe ser mayor o igual a ${minNum}.`;
-// } else if (maxNum !== undefined && valorInput > maxNum) {
-//     error = `El número debe ser menor o igual a ${maxNum}.`;
-// }
--- public/js/moviesList.js
+++ public/js/moviesList.js
@@ -11,3 +11,92 @@ window.onload = function(){
     moviesListTitulo.style.padding = '20px';
 
 }
+
+
+window.onload = function () {
+    // Definir variables globales para controlar los errores
+    let listOfErrors = {};
+
+    let titulo = document.querySelector('.moviesAddTitulo')
+    let formulario = document.querySelector('#formulario');
+    let article = document.querySelector('article');
+    titulo.innerHTML = 'AGREGAR PELÍCULA';
+    titulo.classList.add('titulo');
+    article.classList.add('fondoTransparente');
+    formulario.classList.add('fondoCRUD');
+    let errorsList = document.querySelector('.errores')
+
+    const form = document.querySelector('form')
+
+    const rating = document.querySelector('#rating');
+    const awards = document.querySelector('#awards');
+    const release_date = document.querySelector('#release_date');
+    const length = document.querySelector('#length');
+    const genre = document.querySelector('#genre_id');
+
+    const title = document.querySelector('#title');
+    title.focus();
+
+    function validarInput(input, minLength, maxLength, minNum, maxNum) {
+        return function (event) {
+            const valorInput = input.value.trim();
+            let error = '';
+
+            // Realizar las validaciones necesarias
+            if (valorInput == '' || valorInput == null) {
+                error = `El campo ${input.name} es obligatorio.`;
+                listOfErrors[input.name] = `El campo ${input.name} es obligatorio.`
+            } else {
+                if (minLength && valorInput.length < minLength) {
+                    error = `El campo ${input.name} debe tener al menos ${minLength} caracteres.`;
+                    listOfErrors[input.name] = `El campo ${input.name} debe tener al menos ${minLength} caracteres.`;
+                } else if (maxLength && valorInput.length > maxLength) {
+                    error = `El campo ${input.name} no puede tener más de ${maxLength} caracteres.`;
+                    listOfErrors[input.name] = `El campo ${input.name} no puede tener más de ${maxLength} caracteres.`;
+                }
+            }
+            if (minNum && parseFloat(valorInput) < minNum) {
+                error = `El valor del campo ${input.name} debe ser mayor o igual a ${minNum}.`;
+            } else if (maxNum && parseFloat(valorInput) > maxNum) {
+                error = `El valor del campo ${input.name} debe ser menor o igual a ${maxNum}.`;
+            } else if (valorInput < 0) {
+                error = `El valor del campo ${input.name} no puede ser negativo.`;
+            }
+
+            const mensajeErrorElement = input.nextElementSibling;
+            mensajeErrorElement.textContent = error;
+
+            // Agregar o eliminar la clase 'is-invalid' según si hay error o no
+            if (error) {
+                input.classList.add('is-invalid');
+            } else {
+                input.classList.remove('is-invalid');
+                input.classList.add('is-valid');
+                delete listOfErrors[input.name]
+            }
+        };
+    }
+    title.addEventListener("blur", validarInput(title, 3, 15));
+    length.addEventListener("blur", validarInput(length, null, null, 60, 360));
+    rating.addEventListener("blur", validarInput(rating, null, null, 0, 10));
+    awards.addEventListener("blur", validarInput(awards, null, null, 0, 10));
+    genre.addEventListener('blur', validarInput(genre));
+    release_date.addEventListener('blur', validarInput(release_date))
+
+    form.addEventListener('submit', (event) => {
+
+    validarInput(title, 3, 15);
+    validarInput(length, null, null, 60, 360);
+    validarInput(rating, null, null, 0, 10);
+    validarInput(awards, null, null, 0, 10);
+    validarInput(genre);
+    release_date.addEventListener('blur', validarInput(release_date))
+        if (Object.keys(listOfErrors).length > 0) {
+            event.preventDefault();
+            errorsList.innerHTML = '';
+            for (const key in listOfErrors) {
+                errorsList.innerHTML += `<li>${listOfErrors[key]}</li>`;
+            }
+        }
+    });
+}
